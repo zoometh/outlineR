1eb7de29be97a9f8f813db93a8f8b75c7ab8979a
zoometh <thomashuet7@gmail.com>
2021-07-02 14:46:14 +0200

    separate

--- R/separate_single_artefacts_function.R
+++ R/separate_single_artefacts_function.R
@@ -7,13 +7,17 @@
 #' images with multiple artefacts on it.
 #' @param outpath Pathname to the folder where the JPEGs of
 #' the singled-out artefacts from this function should be stored.
+#' @param thres threshold, by default: 50%. Less contrasted images
+#' and images with objects showing light hues need to have
+#' a higher threshold (~ 80%). Very contrasted images could have a
+#' lesser threshold (~ 20%)
 #'
-  #' @return If return_combined_outlines = TRUE, returns the combined
+#' @return If return_combined_outlines = TRUE, returns the combined
 #' Coo objects in a single Opn file. If return_combined_outlines = FALSE,
 #' returns a list of coordinate matrices of each open outline.
 #'
 #' @export
-separate_single_artefacts <- function(inpath, outpath) {
+separate_single_artefacts <- function(inpath, outpath, thres = "50%") {
 
   files_to_use_names <- list.files(inpath,
                                    full.names = FALSE,
@@ -24,41 +28,55 @@ separate_single_artefacts <- function(inpath, outpath) {
 
   pb <- utils::txtProgressBar(min = 0, max = length(inpath), style = 3)
   for (current_masked_file in 1:length(inpath)) {
-    x_raw <- imager::load.image(inpath[current_masked_file])
-
-    x_grayscale <- imager::grayscale(x_raw)
-
+    # current_masked_file <- 1
+    # x_raw <- imager::load.image(inpath[current_masked_file])
+    x_raw <- magick::image_read(inpath[current_masked_file])
+    # plot(x_raw)
+    test <- magick::image_convert(x_raw, colorspace = "Gray")
+    # plot(test)
+    x_thres <- magick::image_threshold(test,
+                                       type = "black",
+                                       threshold = thres)
+    # plot(x_thres)
+    x_grayscale <- imager::magick2cimg(x_thres)
+    # x_grayscale <- imager::grayscale(x_raw)
+    # plot(x_grayscale)
     # because some images had to be pre-processed in GIMP with the thresholding tool, this step might result in errors
     x_threshold <- x_grayscale
-    try(x_threshold <- imager::threshold(x_threshold),
-        silent = TRUE)
-    x_clean <- imager::clean(x_threshold, 10)
+    # plot(imager::threshold(x_threshold, thr = "20%"))
+    try(x_threshold <- imager::threshold(x_threshold))
+    # plot(x_threshold)
+    x_clean <- imager::clean(x_threshold, 1)
+    # plot(x_clean)
     x_clean_cimg <- imager::as.cimg(x_clean)
 
     x_clean_img <- EBImage::Image(x_clean_cimg)
     x_clean_img_filled <- EBImage::fillHull(1 - x_clean_img)
+    # plot(x_clean_img_filled)
 
     bin_image <- round(x_clean_img_filled / max(x_clean_img_filled), 0)
+    # plot(bin_image)
 
     bin_image_labeled <- EBImage::bwlabel(bin_image)
+    # plot(bin_image_labeled)
 
     # important step to be able to work with images that are already binary and images that are still in grayscale
     if (EBImage::numberOfFrames(bin_image_labeled) == 1) {
       bin_image_labeled_filled_frame <- try(EBImage::getFrame(bin_image_labeled, 1),
-        silent = TRUE
+                                            silent = TRUE
       )
     } else {
       bin_image_labeled_filled_frame <- try(EBImage::getFrame(bin_image_labeled, 3),
-        silent = TRUE
+                                            silent = TRUE
       )
     }
-
+    # plot(bin_image_labeled_filled_frame)
     features <- EBImage::computeFeatures.shape(bin_image_labeled_filled_frame)
 
     all_objects <- 1:max(bin_image_labeled_filled_frame)
 
     for (object_counter in all_objects) {
-
+      # object_counter <- 1
       # single out individual artefacts (starting with the largest)
       current_object <- EBImage::rmObjects(
         bin_image_labeled_filled_frame,
@@ -69,13 +87,15 @@ separate_single_artefacts <- function(inpath, outpath) {
       current_object_inverted <- 1 - current_object
 
       # save it as .jpg with a pseudo-number (does not represent the number which the artefact might have on the page)
+      out.name <- unlist(strsplit(files_to_use_names[current_masked_file],
+                                  split = "[.]"))
+      # remove device (last element)
+      image.name <- paste0(paste0(head(out.name, -1), collapse = "."),
+                           ".out", object_counter,
+                           ".jpg")
       EBImage::writeImage(
         current_object_inverted,
-        file.path(outpath,
-                  paste0(strsplit(files_to_use_names[current_masked_file], split = "[.]")[[1]][1],
-                         "_pseudo_no_",
-                         object_counter,
-                         ".jpg"))
+        file.path(outpath, image.name)
       )
     }
 
